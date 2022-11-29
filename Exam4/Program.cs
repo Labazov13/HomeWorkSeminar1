4bf83a981cf02750fddaaf01aa1e63b732e0bf9f
Nikolay <labazopelastra@gmail.com>
2022-11-30 02:52:36 +0700

    add Exam4

---
+++ Exam4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите третье число");
+int d = int.Parse(Console.ReadLine()!);
+
+if (a > b & a > d)
+{
+    Console.WriteLine("Большее число a, равное " + a);
+}
+else if (b > a & b > d)
+{
+    Console.WriteLine("Большее число b, равное " + b);
+}
+else
+{
+    Console.WriteLine("Большее число d, равное " + d);
+}
