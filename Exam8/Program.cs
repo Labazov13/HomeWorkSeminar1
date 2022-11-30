4960597a9558d545ba973f19dd29049d6117f609
Nikolay <labazopelastra@gmail.com>
2022-11-30 21:38:03 +0700

    add Exam8

---
+++ Exam8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int N=int.Parse(Console.ReadLine()!);
+if(N==1){
+    Console.WriteLine("Четного числа от 1 до "+N+" не может быть");
+}
+else{
+for(int i=1;i<=N;i++){
+    if(i%2==0){
+    Console.WriteLine(i);    
+    }
+}
+}
