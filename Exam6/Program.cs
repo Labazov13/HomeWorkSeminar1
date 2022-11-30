fba3e95073619c52f2cc75ad6c3f90c17c0d86f0
Nikolay <labazopelastra@gmail.com>
2022-11-30 14:06:19 +0700

    add exam6

---
+++ Exam6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int num =int.Parse(Console.ReadLine()!);
+
+if(num%2==0){
+    Console.WriteLine(num+" Четное число");
+}
+else{
+    Console.WriteLine(num+" Нечетное число");
+}
